591bbc384de42706bcb2b317238edb4644351be9
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-27 19:50:05 +0530

    first commit

--- Router/category.js
+++ Router/category.js
@@ -21,7 +21,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'category', // Cloudinary folder name
-        allowed_formats: ['jpg', 'png', 'jpeg'],
+        allowed_formats: ['jpg', 'png', 'jpeg','webp'],
     },
 });
 
@@ -29,29 +29,37 @@ const upload = multer({ storage });
 
 router.post("/create", upload.single("picture"), async (req, res) => {
     try {
+        // Check if the category name already exists
         const existingCategory = await Category.findOne({ name: req.body.name });
-        if (existingCategory){
-            return res.status(400).json({ message: "Category Already Exists" });
-        }
         
+        if (existingCategory) {
+            return res.status(400).json({ message: "Category already exists" });
+        }
+
+        // Ensure a file was uploaded
+        if (!req.file) {
+            return res.status(400).json({ message: "No file uploaded." });
+        }
+
+        // Create a new category
         const category = new Category({
-            ...req.body,
+            name: req.body.name,
             picture: req.file.path, // Assuming this is the correct path for the picture
         });
 
+        // Save the new category to the database
         await category.save();
         
+        // Respond with success
         res.status(201).json({ message: "Category created successfully", category });
     } catch (error) {
-        console.error(error);
+        console.error("Error creating category:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 });
 
-
 router.get('/get', async (req, res) => {
     try {
-        
         const category = await Category.find({});
         if (!category) {
             return res.status(400).json({ message: "Data not found" });
